5f00d9387faf02c417b08d65fa8a744d810bfddf
chad <choman@gmail.com>
2015-11-09 17:54:01 -0600

    extractions should be complete.  Now need to figure out data and  download

--- getcoreco.sh
+++ getcoreco.sh
@@ -11,21 +11,25 @@ IFS="
 for i in `curl -s curl -s http://events.attendthisevent.com/Classic/?eventid=$id | egrep ':title|:description|TargetDate|mp3'`
 do
     if [ -z "${i##*title*}" ] ;then
-        echo "String '$i' contain substring: '$reqsubstr'."
+        title=`echo $i | grep -o ' content=['"'"'"][^"'"'"']*['"'"'"]' | \
+                         sed -e 's/^ content=["'"'"']//' -e 's/["'"'"']$//'`
+        echo "title = ($title)"
     fi
 
     if [ -z "${i##*description*}" ] ;then
-        echo "String '$i' contain substring: '$reqsubstr'."
+        desc=`echo $i | grep -o ' content=['"'"'"][^"'"'"']*['"'"'"]' | \
+                         sed -e 's/^ content=["'"'"']//' -e 's/["'"'"']$//'`
+        echo "desc = ($desc)"
     fi
 
     if [ -z "${i##*TargetDate*}" ] ;then
-        echo "String '$i' contain substring: '$reqsubstr'."
+        echo $i | awk '{print $4}' | sed -e "s/'//"
     fi
 
     if [ -z "${i##*mp3*}" ] ;then
-        mp3=`echo $i |  grep -o ' href=['"'"'"][^"'"'"']*['"'"'"]' | \
-                   sed -e 's/^<a href=["'"'"']//' -e 's/["'"'"']$//'`
-        echo $mp3
+        mp3=`echo $i |  grep -o 'href=['"'"'"][^"'"'"']*['"'"'"]' | \
+                   sed -e 's/href=["'"'"']//' -e 's/["'"'"']$//'`
+        echo "mp3 = ($mp3)"
     fi
 
 done

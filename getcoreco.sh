30d703b518fcb2726859ce2fefbc4d06fdc8afab
chad <choman@gmail.com>
2015-11-09 17:44:22 -0600

    Initial extraction of elements

--- getcoreco.sh
+++ getcoreco.sh
@@ -10,6 +10,23 @@ IFS="
 
 for i in `curl -s curl -s http://events.attendthisevent.com/Classic/?eventid=$id | egrep ':title|:description|TargetDate|mp3'`
 do
-     echo $i
+    if [ -z "${i##*title*}" ] ;then
+        echo "String '$i' contain substring: '$reqsubstr'."
+    fi
+
+    if [ -z "${i##*description*}" ] ;then
+        echo "String '$i' contain substring: '$reqsubstr'."
+    fi
+
+    if [ -z "${i##*TargetDate*}" ] ;then
+        echo "String '$i' contain substring: '$reqsubstr'."
+    fi
+
+    if [ -z "${i##*mp3*}" ] ;then
+        mp3=`echo $i |  grep -o ' href=['"'"'"][^"'"'"']*['"'"'"]' | \
+                   sed -e 's/^<a href=["'"'"']//' -e 's/["'"'"']$//'`
+        echo $mp3
+    fi
+
 done
 
